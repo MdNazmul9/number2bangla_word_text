2d02060f31d8b834d793cbbe428d8278625f9eca
MdNazmul9 <nazmul.cse48@gmail.com>
2021-12-13 16:24:27 +0600

    Digit to Text Convert

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'Convert Number to Bangla Word Text'
 LONG_DESCRIPTION = 'A package that allows number input and convert the number as bangla word text .'
 
